c43cdd9ead6142f58aea4ae00fc4a115c23f327b
Jason Forrester <jason4ester1@gmail.com>
2024-07-02 10:53:02 -0500

    part 5

--- PA-303.2.3/src/main/java/org/example/Main.java
+++ PA-303.2.3/src/main/java/org/example/Main.java
@@ -2,15 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 7;
-        int y = 17;
-        int z = x & y;
-        z = x | y;
-        System.out.println("Task 4:");
-        System.out.println("x:" + x +"(" + Integer.toBinaryString(x) + ")" );
-        System.out.println("x:" + y +"(" + Integer.toBinaryString(y) + ")" );
-        System.out.println("x:" + z +"(" + Integer.toBinaryString(z) + ")" );
-        System.out.println("Binary form: "+ Integer.toBinaryString(x));
+        int a = 5;
+        System.out.println("Task 5:");
+        System.out.println("Before increment: " + a);
+        a++;
+        System.out.println("After increment: " + a);
+        System.out.println();
 
     }
 }
