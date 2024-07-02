a6e0c4c289544167e47b7f27cfd134aee31142e0
Jason Forrester <jason4ester1@gmail.com>
2024-07-02 10:36:58 -0500

    part 4

--- PA-303.2.3/src/main/java/org/example/Main.java
+++ PA-303.2.3/src/main/java/org/example/Main.java
@@ -5,10 +5,12 @@ public class Main {
         int x = 7;
         int y = 17;
         int z = x & y;
-        System.out.println("Task 3:");
+        z = x | y;
+        System.out.println("Task 4:");
         System.out.println("x:" + x +"(" + Integer.toBinaryString(x) + ")" );
         System.out.println("x:" + y +"(" + Integer.toBinaryString(y) + ")" );
         System.out.println("x:" + z +"(" + Integer.toBinaryString(z) + ")" );
         System.out.println("Binary form: "+ Integer.toBinaryString(x));
+
     }
 }
