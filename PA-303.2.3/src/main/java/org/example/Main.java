2289ae1eb996ce8bfe11faaf12f97f55021895b0
Jason Forrester <jason4ester1@gmail.com>
2024-07-02 09:35:29 -0500

    Part one

---
+++ PA-303.2.3/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 9;
+        System.out.println("Original value: " + x);
+        System.out.println("Binary form: "+ Integer.toBinaryString(x));
+        x = x << 1;
+        System.out.println("After left shift by 1:"+ x);
+        System.out.println("Desimal value:" + x);
+        System.out.println("Binary form: " + Integer.toBinaryString(x));
+
+    }
+}
