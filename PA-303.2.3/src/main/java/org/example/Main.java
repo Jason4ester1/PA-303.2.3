d050edf50ae532d8d8eb415f410230c98157a38f
Jason Forrester <jason4ester1@gmail.com>
2024-07-02 09:57:36 -0500

    part two

--- PA-303.2.3/src/main/java/org/example/Main.java
+++ PA-303.2.3/src/main/java/org/example/Main.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 88;
+        int x = 150;
         System.out.println("Original value: " + x);
         System.out.println("Binary form: "+ Integer.toBinaryString(x));
-        x = x << 1;
+        x = x >> 2;
         System.out.println("After left shift by 1:"+ x);
         System.out.println("Desimal value:" + x);
         System.out.println("Binary form: " + Integer.toBinaryString(x));
