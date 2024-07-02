69f63725d8e80644d1e6b3fa524bf03f5671ca08
Jason Forrester <jason4ester1@gmail.com>
2024-07-02 09:46:16 -0500

    part one B

--- PA-303.2.3/src/main/java/org/example/Main.java
+++ PA-303.2.3/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 9;
+        int x = 17;
         System.out.println("Original value: " + x);
         System.out.println("Binary form: "+ Integer.toBinaryString(x));
         x = x << 1;
