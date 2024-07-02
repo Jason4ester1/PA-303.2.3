7b841785fb29310f4befb50d5ed61c18b3352f50
Jason Forrester <jason4ester1@gmail.com>
2024-07-02 13:13:01 -0500

    part 6

--- PA-303.2.3/src/main/java/org/example/Main.java
+++ PA-303.2.3/src/main/java/org/example/Main.java
@@ -2,11 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        System.out.println("Task 5:");
-        System.out.println("Before increment: " + a);
-        a++;
-        System.out.println("After increment: " + a);
+        int b = 10;
+        System.out.println("Task 6:");
+        // Method 1
+        System.out.println("Original value: " + b);
+        b++;
+        System.out.println("After increment (Method 1): " + b);
+        // Method 2
+        b += 1;
+        System.out.println("After increment (Method 2): " + b);
+        // Method 3
+        b = b + 1;
+        System.out.println("After increment (Method 3): " + b);
         System.out.println();
 
     }
